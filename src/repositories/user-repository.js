1e95c3f4351c371d7419281c173d760b78dabd23
Rjjha <rj848jha@gmail.com>
2024-06-21 21:14:11 +0530

    Fix payment service

--- src/repositories/user-repository.js
+++
@@ -1,8 +0,0 @@
-const {User} = require('../models');
-const CrudRepository = require('./crud-reposiotory');
-class UserRepository extends CrudRepository{
-    constructor(){
-       super(User);
-    }
-}
-module.exports=UserRepository;
